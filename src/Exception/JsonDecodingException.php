85ddf371aa6fdc8fd74df7c4c689e23600a3041b
appkit-framework <admin@appkit.biz>
2025-12-19 03:12:31 +0100

    Clean up imports

--- src/Exception/JsonDecodingException.php
+++ src/Exception/JsonDecodingException.php
@@ -2,6 +2,4 @@
 
 namespace AppKit\Json\Exception;
 
-use AppKit\Json\Exception\JsonException;
-
 class JsonDecodingException extends JsonException {}
--- src/Exception/JsonEncodingException.php
+++ src/Exception/JsonEncodingException.php
@@ -2,6 +2,4 @@
 
 namespace AppKit\Json\Exception;
 
-use AppKit\Json\Exception\JsonException;
-
 class JsonEncodingException extends JsonException {}
